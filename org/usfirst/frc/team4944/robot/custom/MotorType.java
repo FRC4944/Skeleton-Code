0cd3412b1198b98ba869ffec6064aa9f0ab74c47
jackson.kull <JacksonKull123@icloud.com>
2018-08-14 14:50:19 -0600

    Removing Unrequired motor types

--- org/usfirst/frc/team4944/robot/custom/MotorType.java
+++ org/usfirst/frc/team4944/robot/custom/MotorType.java
@@ -1,5 +1,5 @@
 package org.usfirst.frc.team4944.robot.custom;
 
 public enum MotorType {
-	Talon, TalonSRX, Jaguar, Victor888, SPARK, VictorSP, VictorSPX
+	Talon, TalonSRX, Jaguar, SPARK, VictorSP, VictorSPX
 }

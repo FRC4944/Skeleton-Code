5fb5e80054f894ddad17e156dbfbca9a068c1049
jackson.kull <JacksonKull123@icloud.com>
2018-08-14 14:26:46 -0600

    Adding Commands to Buttons
    You can now add a command to a when pressed for a button

--- org/usfirst/frc/team4944/robot/custom/XboxController.java
+++ org/usfirst/frc/team4944/robot/custom/XboxController.java
@@ -1,12 +1,65 @@
 package org.usfirst.frc.team4944.robot.custom;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import edu.wpi.first.wpilibj.command.Command;
 
 public class XboxController {
 	Joystick stick;
-	
+	Button A, B, X, Y, leftBumper, rightBumper, leftMenu, rightMenu, leftStick, rightStick;
 	public XboxController(int port) {
 		this.stick = new Joystick(port);
+		this.A = new JoystickButton(stick, 1);
+		this.B = new JoystickButton(stick, 2);
+		this.X = new JoystickButton(stick, 3);
+		this.Y = new JoystickButton(stick, 4);
+		this.leftBumper = new JoystickButton(stick, 5);
+		this.rightBumper = new JoystickButton(stick, 6);
+		this.leftMenu = new JoystickButton(stick, 7);
+		this.rightMenu = new JoystickButton(stick, 8);
+		this.leftStick = new JoystickButton(stick, 9);
+		this.rightStick = new JoystickButton(stick, 10);
+	}
+	
+	public void addCommandToA(Command c) {
+		A.whenPressed(c);
+	}
+	
+	public void addCommandToB(Command c) {
+		B.whenPressed(c);
+	}
+
+	public void addCommandToX(Command c) {
+		X.whenPressed(c);
+	}
+	
+	public void addCommandToY(Command c) {
+		Y.whenPressed(c);
+	}
+	
+	public void addCommandToLeftBumper(Command c) {
+		leftBumper.whenPressed(c);
+	}
+	
+	public void addCommandToRightBumper(Command c) {
+		rightBumper.whenPressed(c);
+	}
+	
+	public void addCommandToLeftMenu(Command c) {
+		leftMenu.whenPressed(c);
+	}
+	
+	public void addCommandToRightMenu(Command c) {
+		rightMenu.whenPressed(c);
+	}
+	
+	public void addCommandToLeftStick(Command c) {
+		leftStick.whenPressed(c);
+	}
+	
+	public void addCommandToRightStick(Command c) {
+		rightStick.whenPressed(c);
 	}
 	
 	public boolean getA() {

7594b89bf24868b29ae78dc8b549f84923b60fb3
jackson.kull <JacksonKull123@icloud.com>
2018-08-14 14:32:19 -0600

    Motor Types Added

--- org/usfirst/frc/team4944/robot/custom/Motor.java
+++ org/usfirst/frc/team4944/robot/custom/Motor.java
@@ -2,4 +2,10 @@ package org.usfirst.frc.team4944.robot.custom;
 
 public class Motor {
 
+	MotorType type;
+	
+	public Motor(MotorType type) {
+		this.type = type;
+	}
+	
 }
---
+++ org/usfirst/frc/team4944/robot/custom/MotorType.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team4944.robot.custom;
+
+public enum MotorType {
+	Talon, TalonSRX, Jaguar, Victor888, DMC60, SD540B, SD540C, SPARK, VictorSP, VictorSPX
+}

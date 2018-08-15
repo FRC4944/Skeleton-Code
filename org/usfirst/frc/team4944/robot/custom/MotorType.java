872961128b0fe7ac0574827f2bc4ad5094b85258
jackson.kull <JacksonKull123@icloud.com>
2018-08-15 10:29:54 -0600

    Removing Comments

--- org/usfirst/frc/team4944/robot/custom/MotorType.java
+++ org/usfirst/frc/team4944/robot/custom/MotorType.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team4944.robot.custom;
-
+/*
+ * These are all of the MotorTypes we use
+ */
 public enum MotorType {
 	Talon, PWMTalonSRX, CANTalonSRX, Jaguar, SPARK, VictorSP, PWMVictorSPX, CANVictorSPX
 }
